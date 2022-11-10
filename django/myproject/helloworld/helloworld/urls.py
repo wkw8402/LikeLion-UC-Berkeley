4ac31ded90c84fccf1e63364abc6f49b2b6675c6
Kun Woo <wkw8402@berkeley.edu>
2022-11-09 21:59:54 -0800

    django hello world

--- django/myproject/helloworld/helloworld/urls.py
+++ django/myproject/helloworld/helloworld/urls.py
@@ -1,4 +1,4 @@
-"""myproject URL Configuration
+"""helloworld URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import myapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', myapp.views.count), 
 ]
+
+#http://127.0.0.1:8000/
+#http://127.0.0.1:8000/1
+#http://127.0.0.1:8000/abc/1
