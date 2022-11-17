0b4f0c37d2c822511487e5f011b1c597964f43c8
Kun Woo <wkw8402@berkeley.edu>
2022-11-16 22:36:32 -0800

    start working on django cafe proproject

---
+++ django/project1/practice_url/board/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def board(request):
+    return render(request, 'board.html')
+
+def boardfirst(request):
+    return render(request, 'boardfirst.html')
---
+++ django/project1/practice_url/product/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def productlist(request):
+    return render(request, 'productlist.html')
+
+def productfirst(request):
+    return render(request, 'productfirst.html')
